26f9af5a5147b0f21105c7062d7e8ea3b9a24cff
Junseo Kim <semiwest2@gmail.com>
2025-12-17 23:30:59 +0900

    chore(fe): Prettier 추가, Tailwind 클래스 자동 정렬 적용
    - prettier, prettier-plugin-tailwindcss 설치
    - .prettierrc 설정 파일 생성
    - Tailwind 클래스명 자동 정렬 적용
    - format 스크립트 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <p className='text-4xl font-bold text-blue-600 bg-yellow-100 p-4'>우리 모두 다빈치</p>
+      <p className="bg-yellow-100 p-4 text-4xl font-bold text-blue-600">
+        우리 모두 다빈치
+      </p>
     </>
   );
 }
