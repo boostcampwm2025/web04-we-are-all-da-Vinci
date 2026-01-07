3f658416544479f904bb7addbcba678f867445fc
kwon204 <dafeg3@naver.com>
2026-01-08 00:44:02 +0900

    fix(be): RedisModule import

--- server/src/play/play.module.ts
+++ server/src/play/play.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PlayGateway } from './play.gateway';
 import { PlayService } from './play.service';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
   providers: [PlayGateway, PlayService],
