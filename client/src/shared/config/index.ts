4d2533df2e9562fa0c8048cc7d21a58ccf48b415
joarthvr <ioi456859@gmail.com>
2026-01-08 17:49:08 +0900

    lint(fe): 밑에 개행 린트 수정

--- client/src/shared/config/index.ts
+++ client/src/shared/config/index.ts
@@ -3,4 +3,3 @@ export { GAME_PHASE, type Phase } from './gamePhase';
 export { PATHS } from './paths';
 export { CLIENT_EVENTS, SERVER_EVENTS } from './socketEvents';
 export { TITLES } from './titles';
-
