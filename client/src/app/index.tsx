5ebbf99ac9797970f9aacf9a99b85b2408badc41
cjy3458 <cjy3458@naver.com>
2026-01-12 17:58:34 +0900

    fix: AppLayout을 Router 컨텍스트 내부로 변경

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,14 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from '@/app/config/router';
-import AppLayout from '@/app/ui/appLayout';
 
 const router = createBrowserRouter(routes);
 
 const App = () => {
   return (
-    <AppLayout>
-      <RouterProvider router={router} />
-    </AppLayout>
+    <RouterProvider router={router} />
   );
 };
 
