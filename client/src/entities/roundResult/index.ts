9792c36fb5c817a943d7d97f025a6839c6f2ccb0
Junseo Kim <semiwest2@gmail.com>
2026-01-13 18:18:24 +0900

    feat(fe): 신규 컴포넌트 재사용을 위한 라운드 결과 export 추가
    - PlayerReplayCard, PlayerReplaysGrid, PromptSection export

--- client/src/entities/roundResult/index.ts
+++ client/src/entities/roundResult/index.ts
@@ -1,6 +1,9 @@
 export type { RoundResult } from './model';
 export { NextRoundIndicator } from './ui/NextRoundIndicator';
 export { PlayerDrawingCard } from './ui/PlayerDrawingCard';
+export { PlayerReplayCard } from './ui/PlayerReplayCard';
+export { PlayerReplaysGrid } from './ui/PlayerReplaysGrid';
+export { PromptSection } from './ui/PromptSectionSection';
 export { RankingList } from './ui/RankingList';
 export { ReferenceImageCard } from './ui/ReferenceImageCard';
 export { RoundResultHeader } from './ui/RoundResultHeader';
