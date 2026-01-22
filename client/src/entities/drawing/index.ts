e1a33c59dfc2d455601fe27d2049da6b1418ada5
joarthvr <ioi456859@gmail.com>
2026-01-22 23:18:46 +0900

    refactor: 인덱스 반영

--- client/src/entities/drawing/index.ts
+++ client/src/entities/drawing/index.ts
@@ -1,3 +1,2 @@
-
 export { RankingPanel } from './ui/RankingPanel';
 export { StaticCanvas } from './ui/StaticCanvas';
--- client/src/entities/roundResult/index.ts
+++ client/src/entities/roundResult/index.ts
@@ -2,4 +2,3 @@ export { PlayerReplayCard } from './ui/PlayerReplayCard';
 export { PlayerReplaysSection } from './ui/PlayerReplaysSection';
 export { PromptSection } from './ui/PromptSectionSection';
 export type { RoundEndResponse } from './model';
-
