8c610925e3a537fb7de871aa78f0dee35df0a582
avocado8 <574ple1231@gmail.com>
2026-01-29 11:06:29 +0900

    fix(fe): practice 스토리북 mockstroke로 수정

--- client/src/features/practice/ui/Practice.stories.tsx
+++ client/src/features/practice/ui/Practice.stories.tsx
@@ -3,6 +3,7 @@ import { Practice } from './Practice';
 import { useGameStore } from '@/entities/gameRoom';
 import { useEffect } from 'react';
 import { MemoryRouter } from 'react-router-dom';
+import { MOCK_STROKES } from '@/test/mocks/mockStrokes';
 
 const meta = {
   title: 'features/practice/Practice',
@@ -21,36 +22,7 @@ const meta = {
       useEffect(() => {
         useGameStore.setState({
           isPracticing: true,
-          practicePrompt: [
-            {
-              points: [
-                [
-                  90, 84, 70, 39, 27, 23, 1, 0, 4, 41, 57, 102, 133, 147, 176,
-                  213, 231, 242, 246, 245, 232, 214, 191, 160, 122, 94, 90, 88,
-                ],
-                [
-                  73, 61, 56, 56, 62, 64, 124, 174, 184, 240, 248, 248, 255,
-                  255, 241, 219, 193, 166, 149, 124, 97, 75, 62, 60, 80, 78, 75,
-                  69,
-                ],
-              ],
-              color: [239, 68, 68],
-            },
-            {
-              points: [
-                [104, 112, 118, 116],
-                [72, 30, 25, 78],
-              ],
-              color: [0, 0, 0],
-            },
-            {
-              points: [
-                [103, 99, 83, 65, 56, 57, 68, 81],
-                [59, 46, 25, 12, 0, 38, 55, 59],
-              ],
-              color: [34, 197, 94],
-            },
-          ],
+          practicePrompt: MOCK_STROKES,
         });
       }, []);
 
