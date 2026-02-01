2cc266a7b9ba23276669df7c17f4f110124ece29
joarthvr <ioi456859@gmail.com>
2026-02-02 03:31:47 +0900

    feat(be): 에러 코드에 다른 탭 관련 오류 추가

--- server/src/common/constants/error-code.ts
+++ server/src/common/constants/error-code.ts
@@ -9,6 +9,7 @@ export const ErrorCode = {
   PLAYER_ATLEAST_TWO: '플레이어가 최소 2명이 필요합니다.',
   PLAYER_NOT_IN_ROOM: '플레이어가 참여 중이지 않습니다.',
   HOST_CAN_NOT_KICKED: '방장을 퇴장시킬 수 없습니다.',
+  DUPLICATE_SESSION: '다른 탭에서 이미 접속 중입니다.',
 
   // PROMPT
   PROMPT_NOT_FOUND: '프롬프트가 존재하지 않습니다.',
