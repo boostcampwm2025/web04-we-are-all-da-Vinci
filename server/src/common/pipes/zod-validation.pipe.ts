d8f7f8be8707768b5642d519100a2c7c1c25ad00
cjy3458 <cjy3458@naver.com>
2026-02-02 17:09:26 +0900

    feat(fe): nestjs validationPipe 커스텀(zod schema 활용)

---
+++ server/src/common/pipes/zod-validation.pipe.ts
@@ -0,0 +1,22 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+import { ZodSchema, ZodError } from 'zod';
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return this.schema.parse(value);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          message: 'Validation failed',
+          errors: error.errors,
+        });
+      }
+      throw error;
+    }
+  }
+}
