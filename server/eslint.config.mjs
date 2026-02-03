aa2cedce4fba438d0511b4760e5f76a1e524978a
cjy3458 <cjy3458@naver.com>
2026-02-03 09:52:47 +0900

    fix(be): redundant eslint 완화

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -32,6 +32,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-assignment': 'warn',
       '@typescript-eslint/no-unsafe-call': 'warn',
       '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-redundant-type-constituents': 'warn',
       '@typescript-eslint/no-unsafe-return': 'warn',
       'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
