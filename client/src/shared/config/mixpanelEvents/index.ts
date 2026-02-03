0c84aac8457fbc86449a2b35f15fc57be11276af
Junseo Kim <semiwest2@gmail.com>
2026-02-03 10:26:52 +0900

    chore(fe): 사용하지 않는 CLICK_INVITE_EMPTY_SLOT 이벤트 제거

--- client/src/shared/config/mixpanelEvents/index.ts
+++ client/src/shared/config/mixpanelEvents/index.ts
@@ -4,7 +4,6 @@ export const MIXPANEL_EVENTS = {
   CLICK_SETTINGROOM_BTN: '방 설정 버튼 클릭',
   CLICK_SETTINGROOM_PROPERTIES: '방 설정 속성',
   CLICK_COPYLINK_BTN: '링크 복사 버튼 클릭',
-  CLICK_INVITE_EMPTY_SLOT: '빈자리 초대 버튼 클릭',
   DRAWING_TIME: '그림 그리기',
   CLICK_UNDO_BTN: '되돌리기 버튼 클릭',
   CLICK_CLEAR_BTN: '초기화 버튼 클릭',
