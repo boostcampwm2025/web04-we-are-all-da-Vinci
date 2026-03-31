ab88790931510c0b0c7e8692c8c622171fdcdbfe
kwon204 <dafeg3@naver.com>
2026-04-01 00:12:56 +0900

    chore(be): .env 설정

--- server-toss/src/mikro-orm.config.ts
+++ server-toss/src/mikro-orm.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "@mikro-orm/mysql";
 import { User } from "./user/user.entity";
 
 export default defineConfig({
-  dbName: process.env.MYSQL_DATABASE ?? "test_db",
-  host: process.env.MYSQL_HOST ?? "localhost",
-  port: parseInt(process.env.MYSQL_PORT ?? "3306", 10),
+  dbName: process.env.MYSQL_DATABASE ?? "daVinci_toss",
+  host: process.env.MYSQL_HOST ?? "127.0.0.1",
+  port: parseInt(process.env.MYSQL_PORT ?? "3306"),
   user: process.env.MYSQL_USER ?? "root",
   password: process.env.MYSQL_PASSWORD ?? "",
   entities: [User],
-  debug: process.env.NODE_ENV === "production" ? false : true,
+  debug: process.env.NODE_ENV !== "production",
 });
