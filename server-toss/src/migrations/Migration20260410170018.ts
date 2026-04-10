ccbe278e2832f960481bfe43f7d9b4fb1a415ece
kwon204 <dafeg3@naver.com>
2026-04-11 02:02:03 +0900

    feat: 바뀐 엔티티 적용 후 새로운 마이그레이션 파일 생성

--- server-toss/src/migrations/Migration20260410170018.ts
+++ server-toss/src/migrations/Migration20260410170018.ts
@@ -1,16 +1,18 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20260403124020 extends Migration {
+export class Migration20260410170018 extends Migration {
 
   override up(): void | Promise<void> {
     this.addSql(`create table \`prompts\` (\`id\` bigint unsigned not null auto_increment primary key, \`strokes\` text not null) default character set utf8mb4 engine = InnoDB;`);
 
     this.addSql(`create table \`daily_prompts\` (\`id\` bigint unsigned not null auto_increment primary key, \`prompt_id\` bigint unsigned not null, \`prompt_date\` date not null) default character set utf8mb4 engine = InnoDB;`);
     this.addSql(`alter table \`daily_prompts\` add index \`daily_prompts_prompt_id_index\` (\`prompt_id\`);`);
+    this.addSql(`alter table \`daily_prompts\` add unique \`daily_prompts_prompt_date_unique\` (\`prompt_date\`);`);
 
     this.addSql(`create table \`rankings\` (\`id\` bigint unsigned not null auto_increment primary key, \`created_at\` datetime not null, \`updated_at\` datetime not null, \`name\` varchar(10) not null, \`strokes\` text not null, \`similarity\` text not null, \`user_id\` bigint not null, \`drawing_id\` bigint not null) default character set utf8mb4 engine = InnoDB;`);
 
     this.addSql(`create table \`users\` (\`id\` bigint unsigned not null auto_increment primary key, \`created_at\` datetime not null, \`updated_at\` datetime not null, \`user_key\` int not null, \`name\` varchar(10) not null, \`gender\` varchar(8) null, \`birthday\` date null) default character set utf8mb4 engine = InnoDB;`);
+    this.addSql(`alter table \`users\` add unique \`users_user_key_unique\` (\`user_key\`);`);
 
     this.addSql(`create table \`point_logs\` (\`id\` bigint unsigned not null auto_increment primary key, \`created_at\` datetime not null, \`updated_at\` datetime not null, \`point_reason\` enum('ad','share','drawing') not null, \`point_amount\` int not null, \`user_id\` bigint unsigned not null) default character set utf8mb4 engine = InnoDB;`);
     this.addSql(`alter table \`point_logs\` add index \`point_logs_user_id_index\` (\`user_id\`);`);
