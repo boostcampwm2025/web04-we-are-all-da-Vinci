175c29fac5ca6eff912c8f6ba1d9bcabb2972323
kwon204 <dafeg3@naver.com>
2026-04-11 14:06:20 +0900

    chore: 더미 데이터 10개->100개로 수정

--- client-toss/src/entities/ranking/ui/RankingList.tsx
+++ client-toss/src/entities/ranking/ui/RankingList.tsx
@@ -3,12 +3,12 @@ import { RankingEntry } from "./RankingEntry";
 
 export const RankingList = () => {
   // 임시 데이터: 삭제 필요
-  const rankings = new Array(10).fill({}).map((_, index) => ({
+  const rankings = new Array(100).fill({}).map((_, index) => ({
     name: `사용자${index}`,
     userId: index,
     drawingId: index,
     rank: index + 1,
-    totalSimilarity: 100 - index * 1.5,
+    totalSimilarity: 100 - index * 0.75,
   }));
   return (
     <List>
