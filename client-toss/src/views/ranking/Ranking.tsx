11efba2c1e9d36b7b1be6293ce2be8a42d0b31b0
kwon204 <dafeg3@naver.com>
2026-04-12 14:12:56 +0900

    refactor: Header shared 폴더에서 분리

--- client-toss/src/views/ranking/Ranking.tsx
+++ client-toss/src/views/ranking/Ranking.tsx
@@ -1,15 +1,22 @@
 import { MyRanking, RankingList } from "@/entities/ranking";
-import { Header } from "@/shared/ui/Header";
-import { Border, Button } from "@toss/tds-mobile";
+import { Border, Button, Top } from "@toss/tds-mobile";
 import { Link } from "react-router-dom";
 
 export const Ranking = () => {
   return (
     <div className="flex flex-col items-center  justify-center">
       <main className="w-full">
-        <Header
-          title="오늘의 TOP100"
-          subTitle="화살표를 눌러 리플레이를 확인할 수 있어요."
+        <Top
+          title={
+            <Top.TitleParagraph role="heading" aria-level={1}>
+              오늘의 TOP100
+            </Top.TitleParagraph>
+          }
+          subtitleBottom={
+            <Top.SubtitleParagraph role="heading" aria-level={2}>
+              화살표를 눌러 리플레이를 확인할 수 있어요.
+            </Top.SubtitleParagraph>
+          }
         />
         <Border />
         <MyRanking rank={1010101} totalSimilarity={34.123} />
