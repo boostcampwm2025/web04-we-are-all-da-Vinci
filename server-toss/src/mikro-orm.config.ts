1cdc60ab4c2c540861e95133cad80e660e3943de
joarthvr <ioi456859@gmail.com>
2026-04-21 22:17:08 +0900

    chore(be): MikroORM 시간 설정 및 전역 em 사용 옵션 추가 - 테스트 환경에서의 em 활용 - 환경마다 달라질 수 있는 시간설정을 UTC로 고정하여 사용

--- server-toss/src/mikro-orm.config.ts
+++ server-toss/src/mikro-orm.config.ts
@@ -1,13 +1,13 @@
-import "dotenv/config";
-import { defineConfig } from "@mikro-orm/mysql";
-import { User } from "./modules/user/user.entity";
-import { Drawing } from "./modules/drawing/drawing.entity";
-import { Prompt } from "./modules/prompt/prompt.entity";
-import { DailyPrompt } from "./modules/prompt/daily-prompt.entity";
-import { PointLog } from "./modules/point/point-log.entity";
-import { AdView } from "./modules/ad/ad-view.entity";
-import { Ranking } from "./modules/ranking/ranking.entity";
 import { Migrator } from "@mikro-orm/migrations";
+import { defineConfig } from "@mikro-orm/mysql";
+import "dotenv/config";
+import { AdView } from "./modules/ad/ad-view.entity";
+import { Drawing } from "./modules/drawing/drawing.entity";
+import { PointLog } from "./modules/point/point-log.entity";
+import { DailyPrompt } from "./modules/prompt/daily-prompt.entity";
+import { Prompt } from "./modules/prompt/prompt.entity";
+import { Ranking } from "./modules/ranking/ranking.entity";
+import { User } from "./modules/user/user.entity";
 
 export default defineConfig({
   dbName: process.env.MYSQL_DATABASE ?? "daVinci_toss",
@@ -17,6 +17,8 @@ export default defineConfig({
   password: process.env.MYSQL_PASSWORD ?? "",
   entities: [User, Drawing, Prompt, DailyPrompt, PointLog, AdView, Ranking],
   debug: process.env.NODE_ENV !== "production",
+  forceUtcTimezone: true, // UTC로 시간 설정 고정
+  allowGlobalContext: true, // 테스트환경의 전역 em 사용을 위한 설정
   extensions: [Migrator],
   migrations: {
     snapshot: process.env.NODE_ENV !== "production",
