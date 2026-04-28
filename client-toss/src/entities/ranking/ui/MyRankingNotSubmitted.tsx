b7a1828679348d982b293d90a750af424e98527a
kwon204 <dafeg3@naver.com>
2026-04-28 21:23:07 +0900

    refactor: CSS 변수를 사용하도록 수정

--- client-toss/src/entities/ranking/ui/MyRankingNotSubmitted.tsx
+++ client-toss/src/entities/ranking/ui/MyRankingNotSubmitted.tsx
@@ -6,8 +6,10 @@ const MyRankingNotSubmitted = () => {
   return (
     <div className="h-42 flex flex-col items-center justify-center px-(--page-px) text-center">
       <div className="flex flex-col items-center justify-center gap-2">
-        <div className="text-#03183275 text-xs font-[400]">내 등수</div>
-        <div className="text-lg font-[700] text-[#031228]">
+        <div className="text-(--color-description) text-xs font-normal">
+          내 등수
+        </div>
+        <div className="text-lg font-bold text-[`#031228`]">
           {NOT_SUBMITTED_MESSAGE}
         </div>
         <Link to="/">
