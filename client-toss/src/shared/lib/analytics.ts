ed79fc28fc56ec30fefeed30fe3e1ac8c4b35b57
cjy3458 <cjy3458@naver.com>
2026-05-03 16:46:10 +0900

    feat: app-in-toss monitoring 구현

--- client-toss/src/shared/lib/analytics.ts
+++ client-toss/src/shared/lib/analytics.ts
@@ -1,9 +1,7 @@
 import { Analytics } from "@apps-in-toss/web-framework";
 
-export const trackClick = (
-  logName: string,
-  params?: Record<string, unknown>,
-) => Analytics.click({ log_name: logName, ...params });
+export const trackClick = (logName: string, params?: Record<string, unknown>) =>
+  Analytics.click({ log_name: logName, ...params });
 
 export const trackImpression = (
   logName: string,
