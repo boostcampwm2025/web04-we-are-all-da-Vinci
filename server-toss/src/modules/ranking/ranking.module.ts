d5926382e637010cdbc18868249b5d2a8feac168
kwon204 <dafeg3@naver.com>
2026-05-10 17:54:21 +0900

    feat: RankingCleanupScheduler 의존성 추가

--- server-toss/src/modules/ranking/ranking.module.ts
+++ server-toss/src/modules/ranking/ranking.module.ts
@@ -2,13 +2,12 @@ import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Module } from "@nestjs/common";
 import { Ranking } from "./ranking.entity";
 import { RankingController } from "./ranking.controller";
-import { RankingSnapshotScheduler } from "./ranking.snapshot.scheduler";
-import { RankingSnapshotService } from "./ranking.snapshot.service";
 import { RankingService } from "./ranking.service";
+import { RankingCleanupScheduler } from "./ranking.cleanup.scheduler";
 
 @Module({
   imports: [MikroOrmModule.forFeature([Ranking])],
   controllers: [RankingController],
-  providers: [RankingService, RankingSnapshotService, RankingSnapshotScheduler],
+  providers: [RankingService, RankingCleanupScheduler],
 })
 export class RankingModule {}
