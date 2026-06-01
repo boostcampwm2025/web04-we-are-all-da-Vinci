40917ce41dd234c408c2062dd38a42d4d649c70a
kwon204 <dafeg3@naver.com>
2026-06-01 16:49:42 +0900

    feat: 인증 외부 클라이언트 인터페이스 정의

---
+++ server-toss/src/modules/auth/auth-client.ts
@@ -0,0 +1,8 @@
+import { LoginDto } from "./dto/login.dto";
+import { TossUserInfo } from "./toss-api.client";
+
+export interface AuthClient {
+  generateToken(dto: LoginDto): Promise<string>;
+  removeAccessByUserKey(userKey: number): Promise<void>;
+  getUserInfo(accessToken: string): Promise<TossUserInfo>;
+}
