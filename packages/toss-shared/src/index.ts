b0861ac710980b3c062c5e1ad5fd6770cff805e6
joarthvr <ioi456859@gmail.com>
2026-06-17 15:52:59 +0900

    feat: @toss/shared에 출석 도메인 스키마 추가

--- packages/toss-shared/src/index.ts
+++ packages/toss-shared/src/index.ts
@@ -1,7 +1,8 @@
 // toss-specific HTTP 통신 스키마 (client-toss ↔ server-toss 공유)
 // Zod 스키마로 정의하여 런타임 검증 + 타입 추론 동시 제공
-export * from "./schemas/chance.schema";
 export * from "./schemas/archive.schema";
+export * from "./schemas/attendance.schema";
+export * from "./schemas/chance.schema";
 export * from "./schemas/drawing.schema";
 export * from "./schemas/mission.schema";
 export * from "./schemas/notification.schema";
