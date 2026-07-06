d218e81d1227ba5f9b077b83a9a5db8e4a64f66f
kwon204 <dafeg3@naver.com>
2026-07-06 16:47:37 +0900

    feat: Mission period Enum에 continuouly 추가

--- server-toss/src/migrations/Migration20260630000000.ts
+++ server-toss/src/migrations/Migration20260630000000.ts
@@ -4,12 +4,19 @@ export class Migration20260630000000 extends Migration {
   override up(): void {
     this.addSql("alter table `missions` add column `increment_step` int null;");
     this.addSql(
+      "alter table `missions` modify column `period` enum('daily','weekly','tutorial','continuously') not null;",
+    );
+    this.addSql(
       "alter table `user_missions` add column `required_count` int null, add column `level` int not null default 0;",
     );
   }
 
   override down(): void {
     this.addSql("alter table `missions` drop column `increment_step`;");
+    this.addSql(
+      "alter table `mission` modify colum `period` enum('daily','weekly','tutorial') not null",
+    );
+
     this.addSql(
       "alter table `user_missions` drop column `required_count`, drop column `level`;",
     );
